316aafac92546df9623fab9256261aa2e27fbf45
Afreda323 <anthonyfreda323@gmail.com>
2017-08-23 18:27:45 -0400

    reduced getcoins to one line

--- src/controllers/coin.js
+++ src/controllers/coin.js
@@ -3,12 +3,10 @@ import CoinApi from '../services/coinapi'
 let polo = new CoinApi()
 
 const coinController = {
-  getCoins: async (req, res) => {
-    const { period } = req.params
-    if (!period) return res.status(400).send('rip')
-    const prices = await polo.getPrices(period)
-    return res.json(prices)
-  }
+  getCoins: async ({ params: { period } }, res) =>
+    !period
+      ? res.status(400).send('rip')
+      : res.json(await polo.getPrices(period))
 }
 
 export default coinController
