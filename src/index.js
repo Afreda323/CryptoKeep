45148f81b58f3c2c7a2b5680c60b47f9032fe5b2
Afreda323 <anthonyfreda323@gmail.com>
2018-01-09 20:15:58 -0500

    Refactor of app

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
 // Npm imports
+import http from 'http'
 import express from 'express'
-import mongoose from 'mongoose'
+import redis from 'redis'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-
-// User imports
-import userRouter from './routes/user'
-import coinsRouter from './routes/coin'
+import SocketIO from 'socket.io'
 
 //.env
 require('dotenv').config()
-const {PORT, MONGO_URI} = process.env
+const { PORT, REDIS_PORT } = process.env
+
+// redis
+const client = redis.createClient(REDIS_PORT)
+console.log(`Redis Connected: ${REDIS_PORT}`)
 
 //express
 const app = express()
+const server = http.Server(app)
 
-//DB
-mongoose.promise = Promise
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log('Mongo connected'))
-  .catch(e => console.log('Mongo failed to connect', e))
-
-//Middleware
+//middleware
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 
-// Routes
-app.use('/api/coins', coinsRouter)
-app.use('/api/user', userRouter)
+//socket.io
+const io = new SocketIO(server)
+
+// handle disconnection
+io.on('connection', socket => {
+  console.log(`Client ${socket.id} connected.`)
+
+  // Coin refresh interval
+  setInterval(() => {
+    io.emit('refresh', 'Hello')
+  }, 3000)
+  
+  // handle disconnection
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} disconnected.`)
+  })
+})
 
 //listen
-app.listen(PORT, function() {
-  console.log(`App up on ${PORT}`)
+server.listen(PORT, function() {
+  console.log(`App Running: ${PORT}`)
 })
--- src/routes/coin.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import coinController from '../controllers/coin'
-
-const router = new Router()
-router.get('/prices/:period', coinController.getCoins)
-
-export default router
